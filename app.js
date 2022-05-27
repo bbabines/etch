58969e9da4f47f035d777e77e7eda81b732fad77
Brad Babines <bbabines@gmail.com>
2022-05-27 18:56:08 -0400

    Mouseover and reset works with hardcoded divs

--- app.js
+++ app.js
@@ -14,16 +14,41 @@ document.getElementById("grid-container").appendChild(gridItem);
 
 
 // Works as intended. 
-const changeColor = () => {
-document.getElementById("grid-item").style.backgroundColor = "black";
-};
-document.getElementById("grid-item").addEventListener("mousedown", changeColor);
+// const changeColor = () => {
+// document.getElementById("grid-item").style.backgroundColor = "black";
+// };
+// document.getElementById("grid-item").addEventListener("mousedown", changeColor);
 
 
-// Works as intended.
+// // Works as intended.
+// const resetColor = () => {
+//     document.getElementById("grid-item").style.backgroundColor = "";
+//     };
+// // Works as intended.
+// document.getElementById("clear").addEventListener("click",resetColor);
+
+
+
+const grids = Array.from(document.getElementsByClassName('lol'));
+
+grids.forEach(grid => {
+  grid.addEventListener("mouseover", function (event){
+      grid.setAttribute('style', 'background-color: black;');
+  })
+});
+
+// class test. Change fxn and Event listener.
+// const changeColor = () => {
+//     grids.style.backgroundColor = "black";
+//     };
+//     grids.addEventListener("mousedown", changeColor)
+
+
+// class test. CLEAR fxn
 const resetColor = () => {
-    document.getElementById("grid-item").style.backgroundColor = "";
+    grids.forEach( grid => {
+        grid.style.backgroundColor = "";
+    });
     };
-// Works as intended.
 document.getElementById("clear").addEventListener("click",resetColor);
 

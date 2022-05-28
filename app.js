8515c14fe640676cc46257ed226314a54e6aba47
Brad Babines <bbabines@gmail.com>
2022-05-27 20:19:26 -0400

    Redone HTM and CSS so grid stays same size

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ let gridItem = document.createElement("div");
 gridItem.className = "grid-item";
 // document.getElementById("grid-container").appendChild(gridItem);
 
+
+
 //  Create a function to create a div's based on a input?
 for (let i=0; i <256; i++) {
 let gridItem = document.createElement("div");
@@ -10,20 +12,15 @@ gridItem.className = "grid-item";
 document.getElementById("grid-container").appendChild(gridItem);
 }
 
-
-
-
-// Variable to create an array works w/ fxn by using forEach for event listener
+// forEach used to make the event listener work on all divs.
 const grids = Array.from(document.getElementsByClassName('grid-item'));
-
 grids.forEach(grid => {
   grid.addEventListener("mouseover", function (event){
     grid.style.backgroundColor = "black";
   })
 });
 
-
-// Reset button works.
+// Reset button utilizing forEach.
 const resetColor = () => {
     grids.forEach( grid => {
         grid.style.backgroundColor = "";
@@ -31,3 +28,10 @@ const resetColor = () => {
     };
 document.getElementById("clear").addEventListener("click",resetColor);
 
+// Working on user input and fxn for box size.
+// const changeGridSize = document.getElementById("change-grid-size");
+// changeGridSize.addEventListener("click", function (input) {
+//     let input = userInput;
+//     let userInput = prompt("Please enter a number for grid size between 1-100.")
+
+// });

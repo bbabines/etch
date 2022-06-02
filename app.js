b32cb7e75719a843b4170a8ffc4a87d9702b21b3
Brad Babines <bbabines@gmail.com>
2022-06-01 22:55:58 -0400

    Grid resetting with changing grid size from user input

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // Global decelerations.
 let gridItem = document.createElement("div");
 gridItem.className = "grid-item";
-// document.getElementById("grid-container").appendChild(gridItem);
+document.getElementById("grid-container").appendChild(gridItem);
 
 
 
-//  Create a function to create a div's based on a input?
-for (let i=0; i <256; i++) {
+// Baseline grid
+for (let i=1; i <256; i++) {
 let gridItem = document.createElement("div");
 gridItem.className = "grid-item";
 document.getElementById("grid-container").appendChild(gridItem);
@@ -20,6 +20,7 @@ grids.forEach(grid => {
   })
 });
 
+
 // Reset button utilizing forEach.
 const resetColor = () => {
     grids.forEach( grid => {
@@ -28,10 +29,27 @@ const resetColor = () => {
     };
 document.getElementById("clear").addEventListener("click",resetColor);
 
-// Working on user input and fxn for box size.
-// const changeGridSize = document.getElementById("change-grid-size");
-// changeGridSize.addEventListener("click", function (input) {
-//     let input = userInput;
-//     let userInput = prompt("Please enter a number for grid size between 1-100.")
+// Fxn for user input grid creation
+const changeGridSize = document.getElementById("change-grid-size-button");
+changeGridSize.addEventListener("click", function () {
+    let userInput = prompt("Please enter a number for grid size between 1-100.")
 
-// });
+    let numberOfDivs = Math.pow(userInput, 2);
+
+    // Resets grid size
+    document.getElementById("grid-container").innerHTML = "";
+    
+    // Number of columns not correct
+    // Event listener DOES NOT WORK on NEW divs
+    for (let i = 0; i < numberOfDivs; i++) {
+            let gridItem = document.createElement("div");
+            gridItem.className = "grid-item";
+            
+            document.getElementById("grid-container").appendChild(gridItem);
+            // This MAY work if grid can be reset instead of adding divs
+            document.getElementById("grid-container").style.setProperty("grid-template-columns", userInput);
+            
+        
+    }
+});
+
